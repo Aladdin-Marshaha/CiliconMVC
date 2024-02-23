0cd5acb92525ce216ba1de1b4daab3145e0b4c4c
Aladdin <aladdin90.se@gmail.com>
2024-02-23 11:44:20 +0100

    02/23

--- CiliconMVC/Controllers/AuthController.cs
+++ CiliconMVC/Controllers/AuthController.cs
@@ -1,14 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CiliconMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CiliconMVC.Controllers;
 
 public class AuthController : Controller
 {
+    [Route("/signup")]
+    [HttpGet]
     public IActionResult SignUp()
+    {
+        var viewModel = new SignUpViewModel();
+        return View(viewModel);
+    }
+
+    [Route("/signup")]
+    [HttpPost]
+    public IActionResult SignUp(SignUpViewModel model)
     {
         return View();
     }
 
+    [Route("/signin")]
     public IActionResult SignIn()
     {
         return View();
