d1913967df47baf40fa78fcd1be7a536e5168010
Aladdin <aladdin90.se@gmail.com>
2024-02-28 10:12:57 +0100

    Details done

--- CiliconMVC/Controllers/AccountController.cs
+++ CiliconMVC/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CiliconMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CiliconMVC.Controllers;
 
@@ -9,4 +10,28 @@ public class AccountController : Controller
     //{
     //    _accountService = accountService;
     //}
+
+    [Route("/account")]
+    public IActionResult Details()
+    {
+        var viewModel = new AccountDetailsViewModel();
+        //viewModel.BasicInfo = _accountService.GetBasicInfo();
+        //viewModel.AddressInfo = _accountService.GetAddressInfo();
+        return View(viewModel);
+    }
+
+    [HttpPost]
+    public IActionResult BasicInto(AccountDetailsViewModel viewModel)
+    {
+        //_accountService.SaveBasicInfo(viewModel.BasicInfo);
+        return RedirectToAction(nameof(Details), viewModel);
+    }
+
+    [HttpPost]
+    public IActionResult AddressInto(AccountDetailsViewModel viewModel)
+    {
+        //_accountService.SaveAddressInfo(viewModel.AddressInfo);
+        return RedirectToAction(nameof(Details), viewModel);
+    }
 }
+
---
+++ CiliconMVC/ViewModels/AccountDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CiliconMVC.Models;
+
+namespace CiliconMVC.ViewModels;
+
+public class AccountDetailsViewModel
+{
+    public string Title { get; set; } = "Account Details";
+    public AccountDetailsBasicInfoModel BasicInfo { get; set; } = new AccountDetailsBasicInfoModel()
+    {
+        FirstName = "Aladdin",
+        LastName = "Marshaha",
+        Email = "aladdin@gmail.com",
+        ProfileImage = "images/profile-imge.svg"
+    };
+    public AccountDetailsAddressInfoModel AddressInfo { get; set; } = new AccountDetailsAddressInfoModel();
+}
+ 
